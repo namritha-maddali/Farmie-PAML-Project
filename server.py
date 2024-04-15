1e152d66217a5295a5c4b866d94747469cd80917
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-04-15 16:06:16 +0530

    server edited3

--- server.py
+++ server.py
@@ -20,6 +20,22 @@ import os
 app = Flask(__name__)
 CORS(app,origins = ["https://localhost:3000"])
 
+disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
+supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
+
+model = CNN.CNN(39)    
+model.load_state_dict(torch.load("models/plant_disease_model_1_latest.pt"))
+model.eval()
+
+def prediction(image_path):
+    image = Image.open(image_path)
+    image = image.resize((224, 224))
+    input_data = TF.to_tensor(image)
+    input_data = input_data.view((-1, 3, 224, 224))
+    output = model(input_data)
+    output = output.detach().numpy()
+    index = np.argmax(output)
+    return index
 
 @app.route("/diseased_detection", methods=["POST"])
 @cross_origin()
@@ -58,7 +74,7 @@ def members4():
 
         # Load the model and make a prediction
         model = CNN.CNN(39)
-        model.load_state_dict(torch.load('plant_disease_model_1_latest.pt', map_location=torch.device('cpu')))
+        model.load_state_dict(torch.load('models/plant_disease_model_1_latest.pt', map_location=torch.device('cpu')))
         prediction = model(img).detach().numpy()
 
         predicted_class_idx = np.argmax(prediction[0])
